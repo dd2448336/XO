9e0a6e52d4b892877987e56f947d213ab39afbed
dd2448336 <128064412+dd2448336@users.noreply.github.com>
2023-03-16 20:14:16 +0500

    Add files via upload

---
+++ apiXO/apiXO/Program.cs
@@ -0,0 +1,110 @@
+
+using apiXO;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+PseudoGame.Initial();
+
+
+app.MapGet("/gettusers", (string name, string pas, string online) =>
+{
+    List<UserPred> preds = new List<UserPred>();
+    if (PseudoGame.Autorization(name, pas) != null)
+        preds = PseudoGame.GetUsers(online == "1");
+    else
+        preds.Add(new UserPred() { Error = "Имя или пароль польователя неверны" });
+
+    return preds;
+
+});
+
+app.MapGet("/reg", (string name, string pass) =>
+{
+    return PseudoGame.Registration(name, pass);
+});
+
+app.MapGet("/auth", (string name, string pass) =>
+{
+    return PseudoGame.Autorization(name, pass);
+});  
+
+app.MapGet("/zayavka", (string name, string pas, string tus) =>
+{
+    List<Zayavka>? zays = new List<Zayavka>();
+    if (PseudoGame.Autorization(name, pas) != null)
+        zays = PseudoGame.SetZayavka( PseudoGame.GetIdByName(name), Convert.ToInt32(tus));
+    return zays;
+});
+
+app.MapGet("/zayavki", (string name, string pas) =>
+{
+    List<Zayavka>? zays = new List<Zayavka>();
+    if (PseudoGame.Autorization(name, pas) != null)
+        zays = PseudoGame.GetZayavki(PseudoGame.GetIdByName( name));
+    return zays;
+});
+
+
+app.MapGet("/startgs", (string name, string pas, string zayid) =>
+{
+    Sess? ses = new Sess();
+    if (PseudoGame.Autorization(name, pas) != null)
+        ses = PseudoGame.StartGS( Convert.ToInt32(zayid));
+    else
+        ses.Error = "Имя или пароль польователя неверны";
+    return ses;
+});
+
+app.MapGet("/step", (string name, string pas, string sesid, string stepcor) =>
+{
+    Sess? ses = new Sess();
+    if (PseudoGame.Autorization(name, pas) != null)
+        ses = PseudoGame.AddStep(PseudoGame.GetIdByName(name), Convert.ToInt32(sesid), (StepCoor)Convert.ToInt32(stepcor)); 
+    else
+        ses.Error = "Имя или пароль польователя неверны";
+    return ses;
+});
+
+app.MapGet("/logout", (string name, string pas) =>
+{
+    UserPred? up = new UserPred();
+    if (PseudoGame.Autorization(name, pas) != null)
+        up = PseudoGame.LogOut(PseudoGame.GetIdByName(name));
+    else
+        up.Error = "Имя или пароль польователя неверны";
+    return up;
+});
+
+
+app.MapGet("/session", (string name, string pas, string uid) =>
+{
+    Sess? sess = new Sess();
+    if (PseudoGame.Autorization(name, pas) != null)
+        sess = PseudoGame.GetUserSession(Convert.ToInt32(uid));
+    else
+        sess.Error = "Имя или пароль польователя неверны";
+    return sess;
+});
+
+app.MapGet("/sessbid", (string name, string pas, string sid) =>
+{
+    Sess? sess = new Sess();
+    if (PseudoGame.Autorization(name, pas) != null)
+        sess = PseudoGame.GetSessionByTD(Convert.ToInt32(sid));
+    else
+        sess.Error = "Имя или пароль польователя неверны";
+    return sess;
+});
+
+app.MapGet("/alses", (string name, string pas) =>
+{
+    List<Sess> sess = new List<Sess>();
+    if (PseudoGame.Autorization(name, pas) != null)
+        sess = PseudoGame.GetAll();
+    else
+        sess.Add(new Sess() { Error = "Имя или пароль польователя неверны" });
+    return sess;
+});
+
+app.Run();
---
+++ apiXO/apiXO/PtBD.cs
@@ -0,0 +1,131 @@
+﻿namespace apiXO
+{
+    public class Zayavka
+    {
+
+        public Zayavka(UserPred mus, UserPred tus)
+        {
+            User1 = mus;
+            User2 = tus;
+        } 
+        public int Id { get; set; }
+        public UserPred  User1 { set; get; }
+        public UserPred User2 { set; get; } 
+
+    } 
+
+    [Serializable]
+    public class PtBD
+    {
+        public PtBD()
+        {
+            Sess = new List<Sess>();
+            Users = new List<User>();
+
+        }
+        public List<Sess> Sess { set; get; }
+        public List<User> Users { set; get; }
+    }
+
+    public class UserPred
+    {
+        public int UserID { get; set; }
+        public string? Name { get; set; }
+        public bool OnLine { get; set; }
+        public bool HasStep { set; get; }
+        public string? Error { set; get; }
+    }
+
+
+    [Serializable]
+    public class User : UserPred
+    {
+        public User()
+        {
+
+        }
+        public User(string nam, string pas)
+        {
+            Name = nam;
+            Password = pas;
+
+        }
+        public string? Password { get; set; }
+
+    }
+
+    [Serializable]
+    public class Sess
+    {
+        public Sess()
+        {
+
+        }
+
+        public Sess(User us1, User us2)
+        {
+            Igrok1.User = us1;
+            Igrok2.User = us2;
+        }
+        public int SessionID { get; set; }
+        public PodSet Igrok1 { set; get; } = new PodSet() { StepType = StepType.Krestik };
+        public PodSet Igrok2 { set; get; } = new PodSet() { StepType = StepType.Nolik };
+        public List<Step> Steps { get; set; } = new List<Step>();
+        public bool IsRunning { get; set; }
+
+        public UserPred? Result { set; get; } = null;
+
+        public string? Error { set; get; }
+
+        public Step GSBC(StepCoor stepcor)
+        {
+            Step? st = Steps.Find(t => t.Coordinate == stepcor);
+            if (st == null)
+                st = new Step() { Error = "пока пусто" };
+            return st;
+        }
+    } 
+
+    public class PodSet
+    {
+        public PodSet() { }
+
+        public UserPred User { set; get; }
+        public StepType StepType { set; get; }
+    } 
+
+    [Serializable]
+    public class Step
+    {
+        public Step()
+        {
+
+        }
+        public int UserID { set; get; }
+        public StepType StepType { get; set; }
+        public StepCoor Coordinate { set; get; }
+        public string? Error { set; get; }
+    }
+
+    [Serializable]
+    public enum StepCoor
+    {
+        vl = 1,
+        vs = 2,
+        vp = 3,
+        sl = 4,
+        ss = 5,
+        sp = 6,
+        nl = 7,
+        ns = 8,
+        np = 9
+    }
+
+
+    [Serializable]
+    public enum StepType
+    {
+        Krestik,
+        Nolik
+    }
+}
